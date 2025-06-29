f26c6474f2645a140482b93cc4eb669336c612af
MT224244 <25514849+MT224244@users.noreply.github.com>
2025-06-30 03:40:55 +0900

    🧑‍💻 一部設定を変更しやすいように変更

--- scripts/deploy/initialize-bucket.ts
+++ scripts/deploy/initialize-bucket.ts
@@ -6,9 +6,30 @@ const bucketName = configuration.Parameters.BUCKET_NAME;
 
 $`aws s3 mb s3://${bucketName} --region ${region} || true`;
 
-const fpath = '.aws-sam/server.env';
-await Bun.write(fpath, Object
+const serverEnvFilePath = '.aws-sam/server.env';
+await Bun.write(serverEnvFilePath, Object
     .entries(configuration.ServerEnvironment)
     .reduce((acc, [k, v]) => `${acc}${k}=${v}\n`, ''),
 );
-$`aws s3 cp ${fpath} s3://${bucketName}/server.env`;
+$`aws s3 cp ${serverEnvFilePath} s3://${bucketName}/server.env`;
+
+const botEnvFilePath = '.aws-sam/bot.env';
+await Bun.write(botEnvFilePath, Object
+    .entries({
+        DISCORD_CHANNEL_ID: configuration.Parameters.DISCORD_CHANNEL_ID,
+        SERVER_START_LOG_REGEXP: configuration.Parameters.SERVER_START_LOG_REGEXP,
+        SERVER_STOP_LOG_REGEXP: configuration.Parameters.SERVER_STOP_LOG_REGEXP,
+        PLAYER_JOINED_LOG_REGEXP: configuration.Parameters.PLAYER_JOINED_LOG_REGEXP,
+        PLAYER_LEFT_LOG_REGEXP: configuration.Parameters.PLAYER_LEFT_LOG_REGEXP,
+        PLAYER_CHAT_LOG_REGEXP: configuration.Parameters.PLAYER_CHAT_LOG_REGEXP,
+        LIST_COMMAND_REGEXP: configuration.Parameters.LIST_COMMAND_REGEXP,
+        AUTO_SHUTDOWN_MINUTES: configuration.Parameters.AUTO_SHUTDOWN_MINUTES,
+        RCON_PORT: configuration.Parameters.RCON_PORT,
+        SERVER_PORT: configuration.Parameters.SERVER_PORT,
+        MOTD: configuration.Parameters.MOTD,
+        CLOUDFLARE_ZONE_ID: configuration.Parameters.CLOUDFLARE_ZONE_ID,
+        CLOUDFLARE_RECORD_ID: configuration.Parameters.CLOUDFLARE_RECORD_ID,
+    })
+    .reduce((acc, [k, v]) => `${acc}${k}=${v}\n`, ''),
+);
+$`aws s3 cp ${botEnvFilePath} s3://${bucketName}/bot.env`;
